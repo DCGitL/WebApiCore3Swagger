1c77a70752a5ed25aba3037eb46715eaf56fee0c
David Chen <davidl5a@hotmail.com>
2021-01-20 11:03:02 -0500

    asymetricJwtKey

--- WebApiCore3Swagger/Installer/SwaggerServiceInstaller.cs
+++ WebApiCore3Swagger/Installer/SwaggerServiceInstaller.cs
@@ -22,88 +22,81 @@ namespace WebApiCore3Swagger.Installer
 
                 g.SwaggerDoc("v2.2", new OpenApiInfo { Title = "v2.2 Core web api", Description = "Swagger core api 2.2", Version = "v2.2" });
 
-               
-                g.OperationFilter<RemoveVersionFromParameter>();
-                g.DocumentFilter<ReplaceVersionWithValueInPath>();
+
+                //g.OperationFilter<RemoveVersionFromParameter>();
+                //g.DocumentFilter<ReplaceVersionWithValueInPath>();
 
                 // g.OperationFilter<AuthenticationHeaderOperationFilter>();
 
-                var bearerSecurityScheme = new OpenApiSecurityScheme
-                {
-                    In = ParameterLocation.Header,
-                    Description = "Please add Bearer with a speace before the key in the below text box => Bearer Z29vZEtleQ==",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer"
+                //Jwt bearer Token
+                string name = "Authorization";
+                string schemeName ="Bearer";
+                string description = "Please add Bearer with a space before the key in the below text box => Bearer Z29vZEtleQ==";
+                var SecurityScheme = GetSecuritySchema(name, schemeName, description);
+                g.AddSecurityDefinition(schemeName, SecurityScheme);
+                var SecurityRequirement = GetSecurityRequirement(schemeName);
+                g.AddSecurityRequirement(SecurityRequirement);
 
 
-                };
-                g.AddSecurityDefinition("Bearer", bearerSecurityScheme);
-
-                var bearerSecurityRequirement = new OpenApiSecurityRequirement {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer",
-
-                            }
-
-                        },
-                        new string[]{}
-
-                    } };
-                g.AddSecurityRequirement(bearerSecurityRequirement);
-
-
-
-
-                var basicSecurityScheme = new OpenApiSecurityScheme
-                {
-
-                    In = ParameterLocation.Header,
-                    Description = "Please add Basic with a speace before the key in the below text box Note username:pasword must be separated by a d colan then 64 bit encoded before sending => Basic Z29vZEtleQ==",
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Basic"
-
-
-                };
-                g.AddSecurityDefinition("Basic", basicSecurityScheme);
-                var basicSecurityRequirement = new OpenApiSecurityRequirement {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Basic"
-                            }
-
-                        },
-                        new string[]{}
-                    } };
-                g.AddSecurityRequirement(basicSecurityRequirement);
+                schemeName = "Basic";
+                description = "Please add Basic with a speace before the key in the below text box Note username:pasword must be separated by a d colan then 64 bit encoded before sending => Basic Z29vZEtleQ==";
+                SecurityScheme = GetSecuritySchema(name, schemeName, description);
+                g.AddSecurityDefinition(schemeName, SecurityScheme);
+                SecurityRequirement = GetSecurityRequirement(schemeName);
+                g.AddSecurityRequirement(SecurityRequirement);
 
                 
-                g.DocInclusionPredicate((version, desc) =>
-                {
-                    if (!desc.TryGetMethodInfo(out MethodInfo methodInfo))
-                        return false;
-                    var versions = methodInfo.DeclaringType.GetCustomAttributes(true)
-                    .OfType<ApiVersionAttribute>()
-                    .SelectMany(attr => attr.Versions);
+                //g.DocInclusionPredicate((version, desc) =>
+                //{
+                //    if (!desc.TryGetMethodInfo(out MethodInfo methodInfo))
+                //        return false;
+                //    var versions = methodInfo.DeclaringType.GetCustomAttributes(true)
+                //    .OfType<ApiVersionAttribute>()
+                //    .SelectMany(attr => attr.Versions);
 
-                    return versions.Any(v => $"v{v.ToString()}" == version);
-                });
+                //    return versions.Any(v => $"v{v.ToString()}" == version);
+                //});
 
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 g.IncludeXmlComments(xmlPath);
 
             });
+        }
+
+
+
+        private OpenApiSecurityScheme GetSecuritySchema(string name, string schemeName, string description )
+        {
+            return new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description =  description,
+                Name = name, 
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = schemeName 
+
+
+            };
+        }
+
+        private OpenApiSecurityRequirement GetSecurityRequirement(string id)
+        {
+            return new OpenApiSecurityRequirement {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = id,
+
+                            }
+
+                        },
+                        new string[]{}
+
+                    } };
         }
     }
 }
