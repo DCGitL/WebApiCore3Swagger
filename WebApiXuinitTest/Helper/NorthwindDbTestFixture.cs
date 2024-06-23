0cdf0d4f653cdcddb959ffee10e5624f20cc6f53
David Chen <davidl5a@hotmail.com>
2024-06-23 09:49:54 -0400

    Add NorthwindDbTestFixture

---
+++ WebApiXuinitTest/Helper/NorthwindDbTestFixture.cs
@@ -0,0 +1,34 @@
+﻿using Adventure.Works._2012.dbContext.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiXuinitTest.Helper
+{
+    public class NorthwindDbTestFixture
+    {
+        public NorthwindContext DbContext { get; private set; }
+
+        public NorthwindDbTestFixture()
+        {
+            // Setup logic (e.g., database connection)
+            var optionsBuilder = new DbContextOptionsBuilder<NorthwindContext>();
+            optionsBuilder.UseInMemoryDatabase(databaseName: "TestDatabase");
+            DbContext = new NorthwindContext(optionsBuilder.Options);
+
+            DbContext.Database.EnsureCreated();
+            DbContext.Seed();
+
+        }
+
+        public void Dispose()
+        {
+            // Teardown logic (e.g., close database connection)
+            DbContext.Database.EnsureDeleted();
+            DbContext.Dispose();
+        }
+    }
+}
---
+++ WebApiXuinitTest/NorthwindDbUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Adventure.Works._2012.dbContext.Northwind.Repository;
+using Adventure.Works._2012.dbContext.ResponseModels;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiXuinitTest.Helper;
+using Xunit;
+
+namespace WebApiXuinitTest
+{
+    public class NorthwindDbUnitTest : IClassFixture<NorthwindDbTestFixture>
+    {
+        private readonly NorthwindDbTestFixture _fixture;
+
+        public NorthwindDbUnitTest(NorthwindDbTestFixture fixture)
+        {
+            _fixture=fixture;
+        }
+
+        [Fact]
+        public async Task GetAllEmployeesShouldHaveThreeEmploees()
+        {
+            var repo = new NorthwindRepository(_fixture.DbContext);
+
+            var result = await repo.GetAllAsyncEmployees();
+            Assert.NotNull(result);
+            Assert.Equal(3, result.Count<ResponseEmployee>());
+
+        }
+    }
+}
